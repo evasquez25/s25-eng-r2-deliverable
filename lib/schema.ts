eedad4e782e9fd87475840aa144df4826af48d1a
Eric Vasquez <evasquezreyes@college.harvard.edu>
2025-02-12 04:33:24 -0500

    schema.ts fix attempt

--- lib/schema.ts
+++ lib/schema.ts
@@ -182,15 +182,13 @@ export type Enums<
     : never;
 
 
-export type CompositeTypes<
-  PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"] | { schema: keyof Database },
-  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database;
-  }
-    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
-    : never = never,
-> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
-  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
-    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never;
+// export type CompositeTypes<
+//   PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"] | { schema: keyof Database },
+//   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+//     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+//     : never = never,
+// > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+//   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+//   : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+//     ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+//     : never;
